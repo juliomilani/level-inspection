e147663447e65063e793ea157f669276f744b913
lucaslameira <lucaslameira@gmail.com>
2019-12-20 16:23:08 -0300

    added cap finde and baseline detector

--- baseline_finder.py
+++ baseline_finder.py
@@ -3,9 +3,10 @@ import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
 #%%
-def show_images(images,titles,color_map='viridis'):
+def show_images(images,titles=['x','x','x','x','x','x'],color_map='viridis'):
     """
     Show images using matplotlib figures
     """
@@ -23,24 +24,33 @@ images_gray = [cv2.cvtColor(im_bgr, cv2.COLOR_BGR2GRAY) for im_bgr in images_bgr
 show_images(images_rgb,images_name,'gray')
 
 
-images_blue = [cv2.split(img_bgr)[0] for img_bgr in images_bgr]
-blue_title = ["blue:"+name for name in images_name]
-show_images(images_blue,blue_title,'gray')
+def get_cap_masks(images):
+    lower = np.array([0, 60, 100])
+    upper = np.array([59, 130, 170])
+    cap_masks = [cv2.inRange(img_rgb, lower, upper) for img_rgb in images]
+    show_images(cap_masks,images_name,'gray')
+    return cap_masks
 
-images_green = [cv2.split(img_bgr)[1] for img_bgr in images_bgr]
-images_red = [cv2.split(img_bgr)[2] for img_bgr in images_bgr]
+def find_cap_bb(cap_masks):
+    cap_bbs = []
+    images = [img for img in images_rgb]
+    for i,cap_mask in enumerate(cap_masks):
+        cnts = cv2.findContours(cap_mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[0]
+        for c in cnts:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+            if len(approx) == 4:
+                (x, y, w, h) = cv2.boundingRect(approx)
+                print("w:%s, y:%s, w:%s, h:%s i:%s" % (x, y, w, h, i))
+                if w > 100:
+                    cv2.rectangle(images[i], (x, y), (x + w, y + h), (0, 255, 0), thickness=3)
+                    cv2.line(images[i], (0, y+h), (images[i].shape[1], y+h), (0, 255, 0), thickness=3)
+                    el = cap_mask.copy()[y:y + h, x:x + w]
+                    pil_im = Image.fromarray(images_rgb[0])
 
-temp = []
-for i,_ in enumerate(images_blue):
-    img_mean = np.mean(images_blue)
-    aux = np.subtract(images_blue[i],images_gray[i]) * (np.subtract(images_blue[i], images_gray[i]) > 0) 
-    temp.append(np.subtract(images_blue[i],images_gray[i]) * (np.subtract(images_blue[i], images_gray[i]) > 0))
-show_images(temp,blue_title,'gray')
+    show_images(images)
 
-edges = [cv2.Canny(img,25,70) for img in images_gray]
-edges_title = ["canny:25,70"+name for name in images_name]
-#show_images(edges,edges_title)
+find_cap_bb(get_cap_masks(images_rgb))
 
 plt.show()
-
-# %%
